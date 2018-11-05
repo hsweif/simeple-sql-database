75fb6e9876cb9ecec36d670e84bc8d9d9d6fc9ab
AlexFxw <alex93179@gmail.com>
2018-11-05 16:47:16 +0800

    文件操作。

---
+++ include/RM_Manager.h
@@ -0,0 +1,27 @@
+#ifndef RM_MANAGER 
+#define RM_MANAGER 
+
+#include "bufmanager/BufPageManager.h"
+#include "fileio/FileManager.h"
+#include "RecordModule/PageHead.h"
+#include <cstring>
+#include<cstdlib>
+#include <string>
+#include<cstdio>
+
+using namespace std;
+class RM_Manager{
+private:
+    char *dataPath;
+    FileManager *fileManager;
+    int fileID;
+    static int RID;
+public:
+    RM_Manager();
+    int createFile(const char* name, int recordSize);
+    bool openFile(const char* name);
+    int closeFile();
+    int deleteFile(const char* name);
+};
+
+#endif // RM_MANAGER
