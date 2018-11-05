2ff1c2769ec93c4f0984db05cfb6b0f15e303538
justwe <2016011325@secoder.net>
2018-11-05 23:01:09 +0800

    添加记录管理模块说明文档

---
+++ src/bitMapTest.cpp
@@ -0,0 +1,14 @@
+#include "../include/utils/MyBitMap.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	MyBitMap *b = new MyBitMap(10<<5,1);
+	b->show();
+	b->setBit(1,1);
+	b->setBit(3,1);
+	b->show();
+	b->initConst();
+	cout<<b->findLeftOne()<<endl;
+	return 0;
+}
