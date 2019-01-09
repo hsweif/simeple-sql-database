eeea01d9128957a3bcee62049328394222e23ecd
AlexFxw <alex93179@gmail.com>
2019-01-09 19:18:29 +0800

    Modify the testcase.

--- TestDataSet/select.sql
+++ TestDataSet/select.sql
@@ -2,6 +2,9 @@ CREATE DATABASE selectDB
 USE DATABASE selectDB
 create table school (id INT,name CHAR(10))
 create table price (id INT,value INT)
-insert into school (1,'qinghua'),(2,'beida'),(3,'fudan')
-insert into price (1,233),(2,666)
+insert into school values (1,'qinghua'),(2,'beida'),(3,'fudan')
+insert into price values (1,233),(2,666)
+select * from school
+select * from school,price where (school.id=price.id)
+select * from school,price where (price.value<300)
 select * from school,price where (school.id=price.id) AND (price.value<300)
